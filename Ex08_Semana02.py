98d1d093e918b9304c296da203608d19c41eaa16
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-08 16:44:17 -0300

    Ex 8 and 9 done

---
+++ Ex08_Semana02.py
@@ -0,0 +1,23 @@
+'''
+Comece executando a atribuição:
+s = '0123456789'
+Agora, escreva expressões usando a string s e o operador de indexação que é avaliado como:
+(a) '0'
+(b) '1'
+(c) '6'
+(d) '8'
+(e) '9'
+'''
+
+s= '0987654321'
+
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[5])
+print(s[6])
+print(s[7])
+print(s[8])
+print(s[9])
---
+++ Ex09_Semana03.py
@@ -0,0 +1,11 @@
+'''
+Primeiro, execute a atribuição
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+Agora, escreva duas expressões Python que são avaliadas, respectivamente, como a primeiro e a última palavras em
+palavras, na ordem do dicionário.
+'''
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+palavras.sort()
+print(palavras[0])
+print(palavras[-1])
+
