6b47c60586996d9d7b7a2a6ebfc01c3a7aaac4a1
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-08 16:25:52 -0300

    Ex 4 and 5 done

--- Ex04_Sem02.py
+++ Ex04_Sem02.py
@@ -10,4 +10,39 @@ Escreva expressões algébricas Python correspondentes aos seguintes comandos:
 (g) O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.
 '''
 
+#(a) A soma dos 5 primeiros inteiros positivos.
 sum = 0
+for i in range(1,11):
+    if i%2==0:
+        sum += i
+        print(i)
+print(sum)
+
+#(b) A idade média de Sara (idade 23), Mark (idade 19) e Fátima (idade 31).
+print((23+19+31)/3)
+
+#(c) O número de vezes que 73 cabe em 403.
+resultado = 403// 73
+print(resultado)
+
+#(d) O resto de quando 403 é dividido por 73
+resultado = 403%73
+print(resultado)
+
+#(e) 2 à 10ª potência.
+print(2**10)
+
+#(f) O valor absoluto da distância entre a altura de Sara (54 polegadas) e a altura de Mark (57 polegadas).
+print(57-54)
+
+#(g) O menor preço entre os seguintes preços: R$ 34,99, R$ 29,95 e R$ 31,50.
+a = 34.99
+b = 29.95
+c = 31;50
+if a < b and a < c:
+    print(a)
+elif b < a and b < c:
+    print(b)
+else:
+    print(c)
+
