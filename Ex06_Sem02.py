11edc3e125df8ebbccef5692858562334946cfd3
Johnny <63070273+TJfiles@users.noreply.github.com>
2022-06-08 16:37:11 -0300

    Ex 6 and 7 done

---
+++ Ex06_Sem02.py
@@ -0,0 +1,10 @@
+'''
+Escreva instruções Python que correspondem às ações a seguir e execute-as:
+(a) Atribua o valor inteiro 3 à variável a.
+(b) Atribua 4 à variável b.
+(c) Atribua à variável c o valor da expressão a * a + b * b.
+'''
+
+a = 3
+b = 4
+c = a*a+b*b
---
+++ Ex07_Semana02.py
@@ -0,0 +1,20 @@
+'''
+Comece executando as instruções de atribuição:
+>>> s1 = 'ant'
+>>> s2 = 'bat'
+>>> s3 = 'cod'
+Escreva expressões Python usando s1, s2 e s3 e os operadores + e * a fim de avaliar para:
+(a) 'ant bat cod'
+(b) ant ant ant ant ant ant ant ant ant ant'
+(c) 'ant bat bat cod cod cod'
+(d) 'ant bat ant bat ant bat ant bat ant bat ant bat ant bat'
+(e) 'batbatcod batbatcod batbatcod batbatcod batbatcod'
+'''
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+print(s1, s2, s3)
+print((s1+' ')*10)
+print(s1,(s2+' ')*2, (s3+' ')*3)
+print((s1+' '+s2+' ')*7)
+print((s2*2+s3+' ')*5)
